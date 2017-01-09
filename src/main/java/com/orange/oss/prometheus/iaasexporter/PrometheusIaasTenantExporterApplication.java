b2dc5b334c35776caced6b47e086491569d5e4a4
Pierre Oblin <pierre.oblin@orange.com>
2017-01-09 10:35:23 +0100

    initial commit

---
+++ src/main/java/com/orange/oss/prometheus/iaasexporter/PrometheusIaasTenantExporterApplication.java
@@ -0,0 +1,12 @@
+package com.orange.oss.prometheus.iaasexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusIaasTenantExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusIaasTenantExporterApplication.class, args);
+	}
+}
